a0efdf8b645db637805e546fc12ffdcb55af4824
Wicky <codeWicky@163.com>
2017-10-12 19:15:23 +0800

    信号崩溃捕捉

---
+++ DWLogger/Dependence/DWCrashCollector/DWCrashCollector.h
@@ -0,0 +1,13 @@
+//
+//  DWCrashCollector.h
+//  DWLogger
+//
+//  Created by Wicky on 2017/10/12.
+//  Copyright © 2017年 Wicky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DWCrashCollector : NSObject
+
+@end
---
+++ DWLogger/Dependence/DWCrashCollector/DWCrashCollector.m
@@ -0,0 +1,13 @@
+//
+//  DWCrashCollector.m
+//  DWLogger
+//
+//  Created by Wicky on 2017/10/12.
+//  Copyright © 2017年 Wicky. All rights reserved.
+//
+
+#import "DWCrashCollector.h"
+
+@implementation DWCrashCollector
+
+@end
